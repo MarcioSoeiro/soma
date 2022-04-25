98261c3128f50bec15c5010db5d198533c66a80f
MarcioSoeiro <91836397+MarcioSoeiro@users.noreply.github.com>
2022-04-24 22:54:24 -0300

    Update soma.c
    inclusão do código para fazer a soma

--- soma.c
+++ soma.c
@@ -1,4 +1,16 @@
-entrada do primeiro número
-entrada do segundo número
-soma entre os dois números
-exibição do resultado
+#include <stdio.h>
+
+int main(void)
+{
+    int v1, v2, soma;
+
+    printf("ditite o valor 1: \n");
+    scanf("%i", &v1);
+
+    printf("ditite o valor 2: \n");
+    scanf("%i", &v2);
+
+    soma = v1 + v2;
+
+    printf("Resultado: %i + %i = %i \n",v1,v2,soma);
+}
